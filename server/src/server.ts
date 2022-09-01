2ba3d93c481fccc3ddbe97c547a9a3b33e3313a1
rsshonjoydas <rsshonjoydas@gmail.com>
2022-09-01 09:20:43 +0600

    ci(server): airbnb style guide setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,4 @@ app.get('/', (req: Request, res: Response) => {
   res.send('<h2>Hello World!</h2>');
 });
 
-app.listen(env.APP_PORT, () =>
-  console.log(`App listening on port ${env.APP_PORT}`)
-);
+app.listen(env.APP_PORT, () => console.log(`App listening on port ${env.APP_PORT}`));
