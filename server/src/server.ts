dde8aec1fc00931c9a70eba0ae147a7ff71ec90d
rsshonjoydas <rsshonjoydas@gmail.com>
2022-09-01 09:14:42 +0600

    ci(server): project init

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from 'express';
+import env from './config';
+
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h2>Hello World!</h2>');
+});
+
+app.listen(env.APP_PORT, () =>
+  console.log(`App listening on port ${env.APP_PORT}`)
+);
